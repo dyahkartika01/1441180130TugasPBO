936db9a7a544e565bb53930a13ade7859b0623fb
Dyah Kartika Dwi A <dyahkartikaa01@gmail.com>
2015-03-25 14:12:44 +0700

    finish nomer 1

--- Praktikum/pertama.java
+++ Praktikum/pertama.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class pertama {
 	public static void main(String[] args) {
-		Scanner masukan = new Scanner (System.in)
+		Scanner masukan = new Scanner (System.in);
 		System.out.println("Masukan Tinggi Badan : ");
 		int angka = masukan.nextInt ();
 
-		if (angka < 100 == 1)
+		if (angka < 100)
 			System.out.println("Tidak Memenuhi Syarat");
 		else {
 			System.out.println("Memenuhi Syarat");
